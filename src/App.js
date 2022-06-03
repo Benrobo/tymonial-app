0a2da655cba8b6d891411b6805aac48ae0b87b4b
Benrobo <alumonabenaiah71@gmail.com>
2022-06-03 15:37:59 -0800

    updated major changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom"
+import { Route } from "react-router";
+import { DataContextProvider } from "./context/DataContext";
+import "./App.css"
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Templates from "./pages/templates";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <DataContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/template" element={<Templates />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </DataContextProvider>
+    </div >
   );
 }
 
